961c52dd58f8fd3e8b71e14d21c079a89ae9b3d9
wangfeng <wangfengxy@sina.cn>
2023-01-07 18:18:26 +0800

    增加DataFrame相关代码

--- cache/cache_kline.go
+++ cache/cache_kline.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"github.com/mymmsc/gox/logger"
 	"github.com/quant1x/quant/models/Cache"
+	"github.com/quant1x/quant/stock"
 	"os"
 	"strings"
 	"syscall"
@@ -51,4 +52,23 @@ func LoadKLine(fc string) []Cache.DayKLine {
 		}
 		return kls
 	}
+}
+
+// LoadDataFrame 加载数据帧
+func LoadDataFrame(code string) *stock.DataFrame {
+	kls := LoadKLine(code)
+	if len(kls) == 0 {
+		return nil
+	}
+	df := new(stock.DataFrame)
+	df.Length = len(kls)
+	for _, v := range kls {
+		df.Date = append(df.Date, v.Date)
+		df.Open = append(df.Open, v.Open)
+		df.Close = append(df.Close, v.Close)
+		df.High = append(df.High, v.High)
+		df.Low = append(df.Low, v.Low)
+		df.Volume = append(df.Volume, v.Volume)
+	}
+	return df
 }
