120fc5122405b6c10df6eb1ed31de93e77a0155d
wangfeng <wangfengxy@sina.cn>
2023-01-10 09:01:10 +0800

    增加部分测试代码

---
+++ utils/pandas/ewm_test.go
@@ -0,0 +1,56 @@
+package pandas
+
+import (
+	"fmt"
+	"testing"
+)
+
+// https://blog.csdn.net/huangguohui_123/article/details/105066646?spm=1001.2101.3001.6650.3&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-3-105066646-blog-99670246.pc_relevant_multi_platform_whitelistv4&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-3-105066646-blog-99670246.pc_relevant_multi_platform_whitelistv4&utm_relevant_index=5
+
+func TestEWM(t *testing.T) {
+	samples := []float64{
+		1, 2, 3, 4, 5, 6, 7, 8, 9,
+	}
+
+	fmt.Println("---1---")
+	t1 := EWM(samples, 5)
+	fmt.Println(t1)
+	//t2 := formula.MA(t1, 5)
+	//fmt.Println(t2)
+	fmt.Println("---2---")
+
+	//fmt.Println("---1---")
+	//t1 := EWM(samples, 5)
+	//fmt.Println(t1)
+	//fmt.Println("---2---")
+
+	// pervious = beta*pervious + float64(1-beta)*v
+	//x := 1.00
+
+	//df=pd.DataFrame({'x':[1,2,3,4,5,6,7,8,9]})
+	//df['y2']=df['x'].ewm(span=5,adjust=False).mean()
+	//print(df)
+	//
+	// span = 5
+	//   x        y2
+	//0  1  1.000000
+	//1  2  1.250000
+	//2  3  1.687500
+	//3  4  2.265625
+	//4  5  2.949219
+	//5  6  3.711914
+	//6  7  4.533936
+	//7  8  5.400452
+	//8  9  6.300339
+	//t2 := x * 6.117055 + (1-x) * 9
+	//x * 6.117055 + 9 - x *9
+	//x * (6.117055 - 9) + 9 = 7.078037
+	/*
+		x8 := (6.117055 - 8) / (5.175583 - 8)
+		fmt.Println("x8=", x8)
+
+		x9 := (7.078037 - 9) / (6.117055 - 9)
+		fmt.Println("x9=", x9)
+	*/
+
+}
---
+++ utils/pandas/gota_test.go
@@ -0,0 +1,38 @@
+package pandas
+
+import (
+	"fmt"
+	"github.com/go-gota/gota/dataframe"
+	"github.com/go-gota/gota/series"
+	"github.com/quant1x/quant/cache"
+	"strings"
+	"testing"
+)
+
+func TestCsv(t *testing.T) {
+	csvStr := `
+Country,Date,Age,Amount,Id
+"United States",2012-02-01,50,112.1,01234
+"United States",2012-02-01,32,321.31,54320
+"United Kingdom",2012-02-01,17,18.2,12345
+"United States",2012-02-01,32,321.31,54320
+"United Kingdom",2012-02-01,NA,18.2,12345
+"United States",2012-02-01,32,321.31,54320
+"United States",2012-02-01,32,321.31,54320
+Spain,2012-02-01,66,555.42,00241
+`
+	df := ReadCSV(strings.NewReader(csvStr))
+	fmt.Println(df)
+	df.SetNames("a", "b", "c", "d", "e")
+	s1 := df.Col("d")
+	fmt.Println(s1)
+
+	fp := cache.GetCache("sz000002")
+	df = dataframe.ReadCSV(fp)
+	fmt.Println(df)
+	closes := df.Col("Close")
+	closes.Median()
+	ma5 := closes.Rolling(5).Mean()
+	series.New(closes, series.Float, "")
+	fmt.Println(ma5)
+}
