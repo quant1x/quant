9ac00c8d95e232ee80f7a2b61e353f5d676e7927
wangfeng <wangfengxy@sina.cn>
2023-04-24 15:55:24 +0800

    新增0号策略

--- indicator/platform.go
+++ indicator/platform.go
@@ -13,8 +13,12 @@ func Platform(df pandas.DataFrame) pandas.DataFrame {
 		CLOSE = df.ColAsNDArray("close") // 收盘价
 		//HIGH    = df.ColAsNDArray("high")   // 最高价
 		//LOW     = df.ColAsNDArray("low")    // 最低价
-		VOL = df.ColAsNDArray("volume") // 成交量
-		//DATALEN = df.Nrow()                 // 数据长度
+		VOL     = df.ColAsNDArray("volume") // 成交量
+		BV      = df.ColAsNDArray("bv")     // 买入量
+		SV      = df.ColAsNDArray("sv")     // 卖出量
+		BA      = df.ColAsNDArray("ba")     // 买入金额
+		SA      = df.ColAsNDArray("sa")     // 卖出金额
+		DATALEN = df.Nrow()                 // 数据长度
 	)
 	//{T02: 平台, V1.0.0 2023-03-08}
 	//BL1:=VOL/REF(VOL,1);
@@ -35,6 +39,7 @@ func Platform(df pandas.DataFrame) pandas.DataFrame {
 	BLL := REF(STL, bln1)
 	//BLVH:=HHV(VOL,BLN),NODRAW;
 	BLVH := HHV(VOL, bln1)
+	BLZC := LLV(STL, bln1)
 	//BLVHN:=BARSLAST(VOL=BLVH),NODRAW;
 	//BLVHN := BARSLAST(VOL.Eq(BLVH))
 	//BLVL:=LLV(VOL,BLVHN),NODRAW;
@@ -45,18 +50,20 @@ func Platform(df pandas.DataFrame) pandas.DataFrame {
 	//SL:VOL/BLVH,NODRAW;
 	SL := VOL.Div(BLVH)
 	//SL21:=BARSLASTCOUNT(SL<=0.50),NODRAW;
-	//SL21 := BARSLASTCOUNT(SL.Lte(0.50))
-	SL21 := BARSSINCEN(SL.Lte(0.50), BLN)
+	//SL20 := BARSSINCEN(SL.Lte(0.50), BLVHN)
+	SL21 := BARSLASTCOUNT(SL.Lte(0.50))
 	//{为REF修复SL21,需要-1}
 	//SL2:=IFF(SL21>0,SL21-1,DRAWNULL),NODRAW;
-	//SL2 := IFF(SL21.Gt(0), SL21.Sub(1), SL21)
+	nan := stat.Repeat(stat.NaN(), DATALEN)
+	null := stat.NewSeries[stat.DType](nan...)
+	SL2 := IFF(SL21.Gt(0), SL21.Sub(1), null)
 	//SL2 := SL21.Sub(1)
 	//SL2 = SL21
-	//SL3:=REF(VOL,SL2);
 	//SL3 := REF(VOL, SL2)
+	SL3 := REF(VOL, SL2)
 	//SLN:=BARSLAST(SL3=VOL);
-	//SLN := BARSLAST(SL3.Eq(VOL))
-	SLN := SL21
+	SLN := BARSLAST(SL3.Eq(VOL))
+	//SLN := SL21
 	//
 	//缩量周期:SLN,NODRAW,COLORYELLOW;
 	//缩量H:REF(STH,SLN+1),DOTLINE,COLORLIRED;
@@ -67,13 +74,36 @@ func Platform(df pandas.DataFrame) pandas.DataFrame {
 	oBlN := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "BLN", BLN)
 	oBlH := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "BLH", BLH)
 	oBlL := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "BLL", BLL)
+	oBlzc := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "BLZC", BLZC)
 	oSL := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "SL", SL)
 	oBLVH := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "BLVH", BLVH)
+	//oBLVHN := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "BLVHN", BLVHN)
 	oSlN := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "SLN", SLN)
+	//oSlNx := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "SLNx", SL20)
 	oSlH := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "SLH", SLH)
 	oSlL := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "SLL", SLL)
 
-	df = df.Join(oBLVH).Join(oBlN).Join(oBlH).Join(oBlL).Join(oSlN).Join(oSlH).Join(oSlL).Join(oSL)
+	//B:倍量H<缩量H AND 倍量支撑=REF(倍量支撑,1) AND CROSS(缩量L,倍量支撑),COLORRED,NODRAW;
+	x1 := BLH.Lte(SLH)
+	//x2 := BLL.Lte(SLL)
+	x2 := BLZC.Eq(REF(BLZC, 1))
+	x3 := CROSS(SLL, BLZC)
+	//x6 := REF(LOW, 1).Lt(BLZC)
+	//x6 := REF(LOW, 1).Lt(BLZC)
+	b1 := x1.And(x2).And(x3)
+	oB1 := pandas.NewSeries(stat.SERIES_TYPE_BOOL, "B1", b1)
+
+	//B1:CROSS(CLOSE,缩量L),NODRAW;
+	b2 := CROSS(CLOSE, SLL)
+	oB2 := pandas.NewSeries(stat.SERIES_TYPE_BOOL, "B2", b2)
+	//B2:CROSS(CLOSE,倍量H) AND CROSS(CLOSE,倍量L),NODRAW;
+	b3 := CROSS(CLOSE, BLH).And(CROSS(CLOSE, BLL))
+	oB3 := pandas.NewSeries(stat.SERIES_TYPE_BOOL, "B3", b3)
+	mb := BA.Div(BV).Div(100)
+	ms := SA.Div(SV).Div(100)
+	oMb := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "mb", mb)
+	oMs := pandas.NewSeries(stat.SERIES_TYPE_DTYPE, "ms", ms)
+	df = df.Join(oBLVH, oBlzc, oBlN, oBlH, oBlL, oSlN, oSlH, oSlL, oSL, oB1, oB2, oB3, oMb, oMs)
 	return df
 }
 
--- indicator/platform_test.go
+++ indicator/platform_test.go
@@ -8,9 +8,22 @@ import (
 
 func TestPlatform(t *testing.T) {
 	code := "600703.sh"
-	code = "000001.sh"
+	code = "603789.sh"
+	code = "sz000506"
+	code = "sh603367"
+	//code = "sz002275"
+	code = "sz002665"
+	code = "sz002528"
+	//code = "sz000892"
+	//code = "sz000905"
+	code = "sh600641"
+	code = "sh688031"
+	code = "sz000988"
+	code = "sh600105"
+	code = "sz002292"
+	code = "sh600354"
 	df := stock.KLine(code)
 	df1 := Platform(df)
 	fmt.Println(df1)
-	df1.WriteCSV("t.csv")
+	_ = df1.WriteCSV("t02.csv")
 }
